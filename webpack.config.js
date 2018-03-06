71a4afc8ecae589e45e6f7ef60fafbf12cc2af25
Ana Reyna <anareymej@gmail.com>
2018-03-05 20:00:08 -0500

    Initial working structure

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const extractStyles = new ExtractTextPlugin('[name].css')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: extractStyles.extract({
+          use: ['css-loader', 'sass-loader'],
+          fallback: 'style-loader',
+          allChunks: true
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    extractStyles
+  ]
+};
