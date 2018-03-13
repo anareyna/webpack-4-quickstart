65f3c4a6f7e65301adb4c01347f26fb6376c07b7
Ana Reyna <anareymej@gmail.com>
2018-03-13 10:50:49 -0500

    layout wip - js

--- src/scripts/landing.js
+++ src/scripts/landing.js
@@ -4,7 +4,7 @@
  * Class for landing data
  */
 
-// import Ajax from './ajax.es6'
+import { tns } from 'tiny-slider/src/tiny-slider.module'
 
 export default class Landing {
   /**
@@ -18,11 +18,7 @@ export default class Landing {
     this._accordions.forEach(accordion =>
       accordion.addEventListener('click', this.collapseAccordion)
     )
-    // this._buttons.forEach(btn => btn.addEventListener('click', evt => this.selectedBrand(evt, btn)));
-  }
-
-  get data() {
-    return this._brands
+    document.addEventListener('DOMContentLoaded', this.runSliders)
   }
 
   collapseAccordion() {
@@ -31,5 +27,26 @@ export default class Landing {
     } else {
       this.classList.add('is-collapsed')
     }
+  }
+  runSliders() {
+    /* eslint-disable no-unused-vars */
+    let forEach = function(array, callback, scope) {
+      for (let i = 0; i < array.length; i++) {
+        callback.call(scope, i, array[i]) // passes back stuff we need
+      }
+    }
+
+    let sliders = document.querySelectorAll('.js-slider')
+    forEach(sliders, function(index, value) {
+      // now fetch YOUR-SLUG from html (use value.getAttribute('data-name') for < IE10 support)
+      let thumbnail = value.dataset.name
+      let slider = tns({
+        container: value,
+        navContainer: '.thumbnails-' + thumbnail,
+        navAsThumbnails: true,
+        mouseDrag: true,
+        // more options ..
+      })
+    })
   }
 }
