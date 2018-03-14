473e63427d1754bf3da181ec0d8fae0c450b6808
Ana Reyna <anareymej@gmail.com>
2018-03-14 12:14:34 -0500

    layout wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const WebpackNotifierPlugin = require('webpack-notifier')
+const path = require('path')
 
 module.exports = {
   module: {
@@ -37,35 +38,30 @@ module.exports = {
           },
         ],
       },
-      { test: /\.svg$/, loader: 'raw-loader' },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65,
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              // the webp option will enable WEBP
-              webp: {
-                quality: 75,
-              },
-            },
-          },
+          'file-loader?name=[name]-[hash].[ext]',
+          // {
+          //   loader: 'image-webpack-loader',
+          //   options: {
+          //     mozjpeg: {
+          //       progressive: true,
+          //       quality: 65,
+          //     },
+          //     // optipng.enabled: false will disable optipng
+          //     optipng: {
+          //       enabled: false,
+          //     },
+          //     pngquant: {
+          //       quality: '65-90',
+          //       speed: 4,
+          //     },
+          //     gifsicle: {
+          //       interlaced: false,
+          //     },
+          //   },
+          // },
         ],
       },
       {
@@ -76,6 +72,11 @@ module.exports = {
         },
       },
     ],
+  },
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, 'src/assets/'),
+    },
   },
   plugins: [
     new HtmlWebPackPlugin({
